329ca4abf7860dce40a5100fd17522be09336d9a
크게접은하늘 <ykorn012@gmail.com>
2018-08-02 02:03:03 +0900

    Update Local_W2W_FWC_Run.py

--- Local_W2W_FWC_Run.py
+++ Local_W2W_FWC_Run.py
@@ -17,10 +17,9 @@ d_p1 = np.array([[0.1, 0], [0.05, 0]])
 C_p1 = np.transpose(np.array([[0, 0.5, 0.05, 0, 0.15, 0], [0.085, 0, 0.025, 0.2, 0, 0]]))
 
 def main():
-    fwc_p1_vm = Local_FWC_Simulator(A_p1, d_p1, C_p1, 1000000)
+    fwc_p1_vm = Local_FWC_Simulator(A_p1, d_p1, C_p1, 3000)
     fwc_p1_vm.DoE_Run(Z=12, M=10)
     fwc_p1_vm.VM_Run(lamda_PLS=0.1, Z=40, M=10)
-
 
 if __name__ == "__main__":
     main()
